83f6b182fdf460c1169358f1a40eb4359ed626b6
Fonju-Boniface <bonifacefonju@gmail.com>
2024-12-09 15:30:09 +0100

    next theme

--- types/theme-types.tsx
+++ types/theme-types.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 type ThemeColors = "Zinc" | "Rose" | "Blue" | "Green" | "Orange"
 interface ThemeColorStateParams{
     themeColor: ThemeColors;
