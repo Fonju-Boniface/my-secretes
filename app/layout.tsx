5176e6122288708c2dfeaf7c2a9355b281db91b4
Fonju-Boniface <bonifacefonju@gmail.com>
2025-01-02 15:30:18 +0100

    sanity edited  sync 1 11

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,97 +67,99 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <ThemeDataProvider><div
-            className="w-[100%] sm:w-[calc(100%-15rem)] relative px-2 py-2 pb-0 mt-[4rem] flex flex-col
+          <ThemeDataProvider>
+            <div
+              className="w-[100%] sm:w-[calc(100%-15rem)] relative px-2 py-2 pb-0 mt-[4rem] flex flex-col
                   justify-center items-center"
-          >
-            <div className="flex justify-start items-start w-[100%] flex-col">
-              <div
-                className={`shadow-xl flex justify-center items-end fixed top-0 z-30 dark:bg-zinc-900
+            >
+              <div className="flex justify-start items-start w-[100%] flex-col">
+                <div
+                  className={`shadow-xl flex justify-center items-end fixed top-0 z-30 dark:bg-zinc-900
                       bg-zinc-100 left-0 sm:left-0 ${isVisible
-                    ? " bg-red-500 left-0"
-                    : "bg-green-600 left-[-100%]"
-                  } sm:h-[100vh] h-[calc(100vh-3rem)]`}
-              >
-                <SideBar />
+                      ? " bg-red-500 left-0"
+                      : "bg-green-600 left-[-100%]"
+                    } sm:h-[100vh] h-[calc(100vh-3rem)]`}
+                >
+                  <SideBar />
 
-              </div>
-              <div
-                className={`shadow-xl w-[12rem] sm:h-[100vh] h-[calc(100vh-3rem)] flex justify-between
+                </div>
+
+                <div
+                  className={`shadow-xl w-[12rem] sm:h-[100vh] h-[calc(100vh-3rem)] flex justify-between
                       items-center flex-col fixed top-0 sm:left-[3rem] background-primary sm:z-10 z-30
                       dark:bg-zinc-900 bg-zinc-100 overflow-y-auto transition-all ${isVisibleS
-                    ? " bg-red-500 left-0"
-                    : "bg-green-600 left-[-12rem]"
-                  }`}
-              >
-                {/* profile */}
-                <Profile />
+                      ? " bg-red-500 left-0"
+                      : "bg-green-600 left-[-12rem]"
+                    }`}
+                >
+                  {/* profile */}
+                  <Profile />
+                </div>
+                {/* topnav */}
+                <TopNav />
               </div>
-              {/* topnav */}
-              <TopNav />
-            </div>
-            <div
-              className="h-[3rem] hover:shadow-lg transition duration-300 text-center flex
+              <div
+                className="h-[3rem] hover:shadow-lg transition duration-300 text-center flex
                     sm:bottom-[-100%] items-center justify-between border-b border-gray-300
                     bg-gradient-to-b from-zinc-200 p-1 pb-3 backdrop-blur-2xl
                     dark:border-neutral-800 dark:from-inherit rounded-md lg:border lg:bg-gray-200
                     dark:bg-zinc-900 w-full fixed bottom-0 left-0 z-[10000]"
-            >
-              <div className="flex justify-center items-end h-[3rem] w-[3rem]">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  onClick={toggleVisibility}
-                  className={`${isVisible && "bg-primary"}`}
-                >
-                  {!isVisible ? (
-                    <Menu
-                      className={`h-[1.2rem] w-[1.2rem] scale-100 transition-all duration-300 dark:-rotate-0
+              >
+                <div className="flex justify-center items-end h-[3rem] w-[3rem]">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={toggleVisibility}
+                    // className={`${isVisible && "bg-primary"}`}
+                  >
+                    {!isVisible ? (
+                      <Menu
+                        className={`h-[1.2rem] w-[1.2rem] scale-100 transition-all duration-300 dark:-rotate-0
                             dark:scale-100`}
-                    />
-                  ) : (
-                    <X
-                      className="h-[1.2rem] w-[1.2rem] rotate-90 scale-100 transition-all duration-300
+                      />
+                    ) : (
+                      <X
+                        className="h-[1.2rem] w-[1.2rem] rotate-90 scale-100 transition-all duration-300
                             dark:-rotate-0 dark:scale-100"
-                    />
-                  )}
-                </Button>
-              </div>
-              <div className="flex justify-center items-end h-[3rem] w-[3rem]">
-                <Progress />
-              </div>
-              <div className="flex justify-center items-end h-[3rem] w-[3rem]">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  onClick={toggleVisibilities}
-                  className={`${isVisibleS && "bg-primary"}`}
-                >
-                  <UserPlus
-                    size={32}
-                    strokeWidth={3}
-                    className="h-[1.2rem] w-[1.2rem] scale-100 transition-all duration-300 dark:-rotate-0
+                      />
+                    )}
+                  </Button>
+                </div>
+                <div className="flex justify-center items-end h-[3rem] w-[3rem]">
+                  <Progress />
+                </div>
+                <div className="flex justify-center items-end h-[3rem] w-[3rem]">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={toggleVisibilities}
+                    // className={`${isVisibleS && "bg-primary"}`}
+                  >
+                    <UserPlus
+                      size={32}
+                      strokeWidth={3}
+                      className="h-[1.2rem] w-[1.2rem] scale-100 transition-all duration-300 dark:-rotate-0
                           dark:scale-100"
-                  />
-                </Button>
+                    />
+                  </Button>
+                </div>
               </div>
-            </div>
-            <Dialog /> {/* Include the Dialog here */}
-            <div className="w-full relative">
-              <main className="min-h-[100vh] overflow-y-auto w-full mb-10">
-                <ScrollProgress />
+              <Dialog /> {/* Include the Dialog here */}
+              <div className="w-full relative">
+                <main className="min-h-[100vh] overflow-y-auto w-full mb-10">
+                  <ScrollProgress />
 
-                {children}
-              </main>
-              {/* 
+                  {children}
+                </main>
+                {/* 
                   
                   */}
-              <div className=" flex justify-center items-center sm:mb-[.3rem] mb-2 mt-7 w-full">
-                {/* footer */}
-                <Footer />
+                <div className=" flex justify-center items-center sm:mb-[.3rem] mb-2 mt-7 w-full">
+                  {/* footer */}
+                  <Footer />
+                </div>
               </div>
             </div>
-          </div>
             <Toaster />
           </ThemeDataProvider>
         </NextThemesProvider>
